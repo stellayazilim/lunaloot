413490aad9ef3688a6419c2f22b143f3c1ece632
kerimCetinbas <a.cetinbas@icloud.com>
2024-04-25 21:31:32 +0300

    aspire

---
+++ LunaLoot.Master.Aspire.AppHost/Program.cs
@@ -0,0 +1,6 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+
+builder.AddProject<Projects.LunaLoot_Master_Api>("MasterApi");
+
+builder.Build().Run();
---
+++ LunaLoot.Master.Infrastructure/Persistence/EFCore/Migrations/20240424173727_identity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LunaLoot.Master.Infrastructure.Persistence.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Addresses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2024, 4, 24, 17, 37, 27, 684, DateTimeKind.Utc).AddTicks(4606),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2024, 4, 24, 14, 21, 20, 315, DateTimeKind.Utc).AddTicks(6050));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Addresses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2024, 4, 24, 17, 37, 27, 684, DateTimeKind.Utc).AddTicks(4421),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2024, 4, 24, 14, 21, 20, 315, DateTimeKind.Utc).AddTicks(5857));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Addresses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2024, 4, 24, 14, 21, 20, 315, DateTimeKind.Utc).AddTicks(6050),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2024, 4, 24, 17, 37, 27, 684, DateTimeKind.Utc).AddTicks(4606));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Addresses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2024, 4, 24, 14, 21, 20, 315, DateTimeKind.Utc).AddTicks(5857),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2024, 4, 24, 17, 37, 27, 684, DateTimeKind.Utc).AddTicks(4421));
+        }
+    }
+}
